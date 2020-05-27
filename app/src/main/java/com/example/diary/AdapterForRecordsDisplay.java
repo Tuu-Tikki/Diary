cf196aaa4b9939a9a9bf3e7e79835350aa8a22d0
Tuu-Tikki <diana.dalakova@gmail.com>
2020-05-28 01:24:41 +0200

    try to make header for RecyclerView

--- app/src/main/java/com/example/diary/AdapterForRecordsDisplay.java
+++ app/src/main/java/com/example/diary/AdapterForRecordsDisplay.java
@@ -11,23 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class AdapterForRecordsDisplay extends
-        RecyclerView.Adapter<AdapterForRecordsDisplay.ViewHolder> {
+        RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView dateTextView;
-            public TextView timeTextView;
-            public TextView pressureTextView;
-            public TextView pulseTextView;
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-
-                dateTextView = (TextView) itemView.findViewById(R.id.recordDate);
-                timeTextView = (TextView) itemView.findViewById(R.id.recordTime);
-                pressureTextView = (TextView) itemView.findViewById(R.id.recordPressure);
-                pulseTextView = (TextView) itemView.findViewById(R.id.recordPulse);
-            }
-        }
+        private static final int TYPE_HEADER = 0;
+        private static final int TYPE_RECORD = 1;
 
         private List<BloodPressureData> memberRecords;
 
@@ -35,35 +22,102 @@ public class AdapterForRecordsDisplay extends
             memberRecords = records;
         }
 
+        /*public class ViewHolder extends RecyclerView.ViewHolder {
+            public ViewHolder(View view){
+                super(view);
+            }
+        }*/
+
         @Override
-        public AdapterForRecordsDisplay.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             Context context = parent.getContext();
             LayoutInflater inflater = LayoutInflater.from(context);
 
-            View recordView = inflater.inflate(R.layout.item_pressure_records, parent, false);
-
-            ViewHolder viewHolder = new ViewHolder(recordView);
-            return viewHolder;
+            if (viewType == TYPE_RECORD) {
+                View recordView = inflater.inflate(R.layout.item_pressure_records, parent, false);
+                RecordViewHolder viewHolder = new RecordViewHolder(recordView);
+                return viewHolder;
+            } else if (viewType == TYPE_HEADER) {
+                View headerView = inflater.inflate(R.layout.item_pressure_records, parent, false);
+                HeaderViewHolder viewHolder = new HeaderViewHolder(headerView);
+                return viewHolder;
+            }
+            else return null;
         }
 
         @Override
-        public void onBindViewHolder(AdapterForRecordsDisplay.ViewHolder viewHolder, int position) {
-            BloodPressureData record = memberRecords.get(position);
+        public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, int position) {
+            if (viewHolder instanceof HeaderViewHolder) {
+                HeaderViewHolder headerViewHolder = (HeaderViewHolder) viewHolder;
 
-            TextView textViewDate = viewHolder.dateTextView;
-            textViewDate.setText(record.dateOfRecord);
-            TextView textViewTime = viewHolder.timeTextView;
-            textViewTime.setText(record.timeOfRecord);
-            TextView textViewPressure = viewHolder.pressureTextView;
-            String stringPressure = record.systolicPressure + "/" + record.diastolicPressure;
-            textViewPressure.setText(stringPressure);
-            TextView textViewPulse = viewHolder.pulseTextView;
-            String stringPulse = String.valueOf(record.pulse);
-            textViewPulse.setText(stringPulse);
+                TextView textViewDate = headerViewHolder.dateTextView;
+                textViewDate.setText("Date            ");
+                TextView textViewTime = headerViewHolder.timeTextView;
+                textViewTime.setText("Time ");
+                TextView textViewPressure = headerViewHolder.pressureTextView;
+                textViewPressure.setText("Result ");
+                TextView textViewPulse = headerViewHolder.pulseTextView;
+                textViewPulse.setText("Pulse");
+            }
+            else if (viewHolder instanceof RecordViewHolder) {
+                BloodPressureData record = memberRecords.get(position-1);
+                RecordViewHolder recordViewHolder = (RecordViewHolder) viewHolder;
+
+                TextView textViewDate = recordViewHolder.dateTextView;
+                textViewDate.setText(record.dateOfRecord);
+                TextView textViewTime = recordViewHolder.timeTextView;
+                textViewTime.setText(record.timeOfRecord);
+                TextView textViewPressure = recordViewHolder.pressureTextView;
+                String stringPressure = record.systolicPressure + "/" + record.diastolicPressure;
+                textViewPressure.setText(stringPressure);
+                TextView textViewPulse = recordViewHolder.pulseTextView;
+                String stringPulse = String.valueOf(record.pulse);
+                textViewPulse.setText(stringPulse);
+            }
         }
 
         @Override
         public int getItemCount() {
-            return memberRecords.size();
+            return memberRecords.size()+1;
         }
+
+        @Override
+        public int getItemViewType(int position) {
+            if (0 == position) {
+            return TYPE_HEADER;
+            }
+            return TYPE_RECORD;
+        }
+
+        private class HeaderViewHolder extends RecyclerView.ViewHolder {
+            public TextView dateTextView;
+            public TextView timeTextView;
+            public TextView pressureTextView;
+            public TextView pulseTextView;
+
+            public HeaderViewHolder(View headerView) {
+                super(headerView);
+
+                dateTextView = (TextView) headerView.findViewById(R.id.recordDate);
+                timeTextView = (TextView) headerView.findViewById(R.id.recordTime);
+                pressureTextView = (TextView) headerView.findViewById(R.id.recordPressure);
+                pulseTextView = (TextView) headerView.findViewById(R.id.recordPulse);
+            }
+        }
+
+        public class RecordViewHolder extends RecyclerView.ViewHolder {
+            public TextView dateTextView;
+            public TextView timeTextView;
+            public TextView pressureTextView;
+            public TextView pulseTextView;
+
+            public RecordViewHolder(View recordView) {
+                super(recordView);
+
+                dateTextView = (TextView) recordView.findViewById(R.id.recordDate);
+                timeTextView = (TextView) recordView.findViewById(R.id.recordTime);
+                pressureTextView = (TextView) recordView.findViewById(R.id.recordPressure);
+                pulseTextView = (TextView) recordView.findViewById(R.id.recordPulse);
+        }
+    }
 }
